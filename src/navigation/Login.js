8d94cdf19b8349c289c0789567e67b2f34a1574f
luisruelas <luis.ruelas@edu.uag.mx>
2022-08-15 09:49:27 -0700

    Add Login UI

--- src/navigation/Login.js
+++ src/navigation/Login.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import {
+    TextInput,
+    View,
     Text,
-    View
+    Image,
+    TouchableOpacity
 } from 'react-native';
+import CommonStyleSheet from '../styles/common'
 
 export default class Login extends React.Component {
+
+    navigateTo(screenName) {
+        this.props.navigation.push(screenName)
+    }
+
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Login Screen</Text>
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', margin: 100 }}>
+                <Image source={require('../images/common/logo.png')} />
+                <TextInput
+                    style={CommonStyleSheet['form-control']}
+                    placeholder="User"
+                />
+                <TextInput
+                    style={CommonStyleSheet['form-control']}
+                    placeholder="Password"
+                />
+                <TouchableOpacity
+                    onPress={() => this.navigateTo('Cashier')}
+                    style={CommonStyleSheet.btn}
+                >
+                    <Text>Login</Text>
+                </TouchableOpacity>
             </View>
         );
     }
